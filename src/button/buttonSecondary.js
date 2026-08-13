8c44d81e71ea53e4356be6b6e2cac208f2d77dff
Mateus <mateushgds75@gmail.com>
2026-08-13 16:23:59 -0400

    Adicionando o botão para efetuar a limpeza

---
+++ src/button/buttonSecondary.js
@@ -0,0 +1,10 @@
+const buttonLimpar = document.getElementById("limpar");
+
+buttonLimpar.addEventListener("click", () => {
+    const textOne = document.getElementById('algoritmo1');
+    const textTwo = document.getElementById('algoritmo2');
+    const buttonEscolher = document.getElementById('escolher');
+    buttonEscolher.textContent = 'Escolher';
+    textOne.value = '';
+    textTwo.value = '';
+})
