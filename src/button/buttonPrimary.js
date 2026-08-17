7c5acb9faf377d1eafec130c9fb599ca6e151ed5
Mateus Henrique <mateushgds75@gmail.com>
2026-08-16 23:19:23 -0300

    Começando a tratar o resultado

--- src/button/buttonPrimary.js
+++ src/button/buttonPrimary.js
@@ -1,13 +1,14 @@
 import { getDadosAlgoritmo1, getDadosAlgoritmo2 } from '../inputs/inputText.js';
+import Resposta from "../button/result.js"
 
 const btn = document.getElementById('executar');
-
 //logica botao primario
 btn.addEventListener('click', () => {
 
     const dadosOne = getDadosAlgoritmo1();
     const dadosTwo = getDadosAlgoritmo2();
     if (dadosOne.dados.trim() !== '' && dadosTwo.dados.trim() !== '') {
+        Resposta()
         alert(`Os valores dos inputs é: ${dadosOne.dados} / ${dadosTwo.dados}`);
     } else {
         alert('Por favor, insira um valor no input antes de enviar.');
